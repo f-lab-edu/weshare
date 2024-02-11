1a55a03b540063150230273beb5479f59fbf0821
flown222 <jangu3384@gmail.com>
2024-02-11 18:15:58 +0900

    Init: spring boot 초기 설정

---
+++ src/main/java/com/flab/weshare/WeshareApplication.java
@@ -0,0 +1,13 @@
+package com.flab.weshare;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeshareApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WeshareApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/flab/weshare/WeshareApplicationTests.java
@@ -0,0 +1,13 @@
+package com.flab.weshare;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeshareApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
