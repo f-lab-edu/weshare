f34db7349ec61c78ac04af625cc44fa61b3b3636
flown222 <jangu3384@gmail.com>
2024-02-14 15:44:17 +0900

    Feat: 회원가입 필드 정규표현식 검증 기능 추가

---
+++ src/main/java/com/flab/weshare/utils/RegEx.java
@@ -0,0 +1,16 @@
+package com.flab.weshare.utils;
+
+import lombok.Getter;
+
+@Getter
+public class RegEx {
+	public static class Pattern {
+		public static final String NICKNAME_PATTERN = "^[가-힣A-Za-z0-9]{2,6}$";
+		public static final String TELEPHONE_PATTERN = "^01(?:0|1|[6-9])(\\d{3}|\\d{4})\\d{4}$";
+	}
+
+	public static class Message {
+		public static final String NICKNAME_MESSAGE = "닉네임 형식에 맞지 않습니다.";
+		public static final String TELEPHONE_MESSAGE = "전화번호 형식에 맞지 않습니다.";
+	}
+}
