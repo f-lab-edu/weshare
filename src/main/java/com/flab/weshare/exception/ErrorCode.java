1f0acf9faa156bd49822e6d3aed2774d6954cbba
flown222 <jangu3384@gmail.com>
2024-02-16 20:47:50 +0900

    Feat: 런타임 예외 공통 처리 기능 추가

--- src/main/java/com/flab/weshare/exception/ErrorCode.java
+++ src/main/java/com/flab/weshare/exception/ErrorCode.java
@@ -4,9 +4,10 @@ import lombok.Getter;
 
 @Getter
 public enum ErrorCode {
-	DUPLICATE_EMAIL("duplicate_email","이미 존재하는 이메일입니다."),
-	DUPLICATE_NICKNAME("duplicate_nickname","이미 존재하는 닉네임입니다."),
-	INVALID_INPUT("invalid_input","입력값이 올바르지 않습니다.");
+	INTRENAL_SERVER_ERROR("internal_server_error", "서버에 에러가 발생했습니다."),
+	DUPLICATE_EMAIL("duplicate_email", "이미 존재하는 이메일입니다."),
+	DUPLICATE_NICKNAME("duplicate_nickname", "이미 존재하는 닉네임입니다."),
+	INVALID_INPUT("invalid_input", "입력값이 올바르지 않습니다.");
 
 	private final String errorCode;
 	private final String errorMessage;
--- src/main/java/com/flab/weshare/exception/advice/ControllerAdvice.java
+++ src/main/java/com/flab/weshare/exception/advice/ControllerAdvice.java
@@ -11,18 +11,25 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 import com.flab.weshare.domain.base.BaseResponse;
 import com.flab.weshare.domain.base.ErrorResponse;
 import com.flab.weshare.exception.ErrorCode;
-import com.flab.weshare.exception.exceptions.CommonException;
+import com.flab.weshare.exception.exceptions.DuplicateException;
 
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
 @RestControllerAdvice
 public class ControllerAdvice {
+	@ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+	@ExceptionHandler(RuntimeException.class)
+	public BaseResponse commonExceptionHandler(RuntimeException runtimeException) {
+		log.error("exception :", runtimeException);
+		return BaseResponse.fail(ErrorResponse.of(ErrorCode.INTRENAL_SERVER_ERROR));
+	}
+
 	@ResponseStatus(HttpStatus.BAD_REQUEST)
-	@ExceptionHandler(CommonException.class)
-	public BaseResponse commonExceptionHandler(CommonException commonException) {
-		log.error("exception :", commonException);
-		return BaseResponse.fail(ErrorResponse.of(commonException.getErrorCode()));
+	@ExceptionHandler(DuplicateException.class)
+	public BaseResponse commonExceptionHandler(DuplicateException duplicateException) {
+		log.error("exception :", duplicateException);
+		return BaseResponse.fail(ErrorResponse.of(duplicateException.getErrorCode()));
 	}
 
 	@ResponseStatus(HttpStatus.BAD_REQUEST)
