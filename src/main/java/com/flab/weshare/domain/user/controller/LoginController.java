fca3bbe3f3b880284a3423f5cf9c538049936e8c
goodchoi <jangu3384@gmail.com>
2024-02-19 17:35:03 +0900

    Fix: uri 수정

--- src/main/java/com/flab/weshare/domain/user/controller/LoginController.java
+++ src/main/java/com/flab/weshare/domain/user/controller/LoginController.java
@@ -14,7 +14,7 @@ import com.flab.weshare.domain.user.service.UserService;
 import lombok.RequiredArgsConstructor;
 
 @RestController
-@RequestMapping("/api/")
+@RequestMapping("/api")
 @RequiredArgsConstructor
 public class LoginController {
 	private final UserService userService;
