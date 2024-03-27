22f806709fd83d21b91e3cfffcbf6b197669bbe8
flown222 <jangu3384@gmail.com>
2024-03-27 15:41:18 +0900

    Feat: PartyJoin 대기 상태 엔티티 조회 쿼리 및 업데이트 쿼리 추가

--- src/main/java/com/flab/weshare/domain/party/repository/PartyJoinRepository.java
+++ src/main/java/com/flab/weshare/domain/party/repository/PartyJoinRepository.java
@@ -1,8 +1,25 @@
 package com.flab.weshare.domain.party.repository;
 
-import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.List;
 
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import com.flab.weshare.domain.party.entity.Ott;
 import com.flab.weshare.domain.party.entity.PartyJoin;
 
 public interface PartyJoinRepository extends JpaRepository<PartyJoin, Long> {
+	@Query("select pj "
+		+ "from PartyJoin pj "
+		+ "where pj.ott =:ott and pj.partyJoinStatus = com.flab.weshare.domain.party.entity.PartyJoinStatus.WAITING "
+		+ "order by pj.createdDate asc")
+	List<PartyJoin> findWaitingPartyJoinByOtt(@Param("ott") Ott ott);
+
+	@Modifying
+	@Query("update PartyJoin pj "
+		+ "set pj.partyJoinStatus= com.flab.weshare.domain.party.entity.PartyJoinStatus.PAY_WAITING"
+		+ " where pj=:partyJoin")
+	void updatePartyJoinPayWaiting(@Param("partyJoin") PartyJoin partyJoin);
 }
