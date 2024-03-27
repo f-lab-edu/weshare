6d1adcb5723890c0ce985d56a01cfca9e18d31cd
flown222 <jangu3384@gmail.com>
2024-03-27 15:40:21 +0900

    Feat: PartyCapsule  Empty 상태 엔티티 조회 쿼리 및 업데이트 쿼리추가

--- src/main/java/com/flab/weshare/domain/party/repository/PartyCapsuleRepository.java
+++ src/main/java/com/flab/weshare/domain/party/repository/PartyCapsuleRepository.java
@@ -1,8 +1,26 @@
 package com.flab.weshare.domain.party.repository;
 
-import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.List;
 
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import com.flab.weshare.domain.party.entity.Ott;
 import com.flab.weshare.domain.party.entity.PartyCapsule;
+import com.flab.weshare.domain.user.entity.User;
 
 public interface PartyCapsuleRepository extends JpaRepository<PartyCapsule, Long> {
+	@Query("select pc "
+		+ "from PartyCapsule pc "
+		+ "where pc.party.ott =:ott and pc.partyCapsuleStatus = com.flab.weshare.domain.party.entity.PartyCapsuleStatus.EMPTY "
+		+ "order by pc.createdDate asc")
+	List<PartyCapsule> findEmptyCapsuleByOtt(@Param("ott") Ott ott);
+
+	@Modifying
+	@Query("update PartyCapsule pc "
+		+ "set pc.partyMember=:user , pc.partyCapsuleStatus = com.flab.weshare.domain.party.entity.PartyCapsuleStatus.OCCUPIED"
+		+ " where pc=:partyCapsule")
+	void updatePartyCapusuleOccupy(@Param("user") User user, @Param("partyCapsule") PartyCapsule partyCapsule);
 }
