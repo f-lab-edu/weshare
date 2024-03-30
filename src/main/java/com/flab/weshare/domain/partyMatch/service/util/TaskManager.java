003abbdb127690496120f152d736efc16f079b42
DongHyeon Choi <jangu3384@gmail.com>
2024-03-30 17:24:47 +0900

    Chore: 로그 제거

--- src/main/java/com/flab/weshare/domain/partyMatch/service/util/TaskManager.java
+++ src/main/java/com/flab/weshare/domain/partyMatch/service/util/TaskManager.java
@@ -7,18 +7,15 @@ import org.springframework.stereotype.Component;
 
 import lombok.extern.slf4j.Slf4j;
 
-@Slf4j
 @Component
 public class TaskManager<T> {
 	private Set<T> taskSet = ConcurrentHashMap.newKeySet();
 
 	public boolean addTask(T task) {
-		log.info("{} 번째 ottID 작업 시작", task);
 		return taskSet.add(task);
 	}
 
 	public void removeTask(T task) {
-		log.info("{} 번째 ottID 작업 종료", task);
 		taskSet.remove(task);
 	}
 }
