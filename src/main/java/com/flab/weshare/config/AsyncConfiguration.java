664f506cf3a49136a0323fffc8210a0c61356335
goodchoi <jangu3384@gmail.com>
2024-04-06 21:44:01 +0900

    Refactor: 비동기 설정 클래스 ConfigurationProperties 적용

--- src/main/java/com/flab/weshare/config/AsyncConfiguration.java
+++ src/main/java/com/flab/weshare/config/AsyncConfiguration.java
@@ -2,32 +2,28 @@ package com.flab.weshare.config;
 
 import java.util.concurrent.Executor;
 
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.Configuration;
+import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.scheduling.annotation.AsyncConfigurer;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+import org.springframework.stereotype.Component;
 
+import lombok.Setter;
+
+@Setter
 @EnableAsync
-@Configuration
+@Component
+@ConfigurationProperties(prefix = "async")
 public class AsyncConfiguration implements AsyncConfigurer {
-	@Value(value = "async.prefix")
-	private static String threadNamePrefix;
-
-	@Value(value = "async.corePoolSize")
-	private static int corePoolSize;
-
-	@Value(value = "async.maxPoolSize")
-	private static int maxPoolSize;
-
-	@Value(value = "async.queueCapacity")
-	private static int queueCapacity;
-
-	@Value(value = "async.keepAliveSeconds")
-	private static int keepAliveSeconds;
+	private String threadNamePrefix;
+	private int corePoolSize;
+	private int maxPoolSize;
+	private int queueCapacity;
+	private int keepAliveSeconds;
 
 	@Override
 	public Executor getAsyncExecutor() {
+
 		ThreadPoolTaskExecutor threadPoolTaskExecutor = new ThreadPoolTaskExecutor();
 		threadPoolTaskExecutor.setThreadNamePrefix(threadNamePrefix);
 		threadPoolTaskExecutor.setCorePoolSize(corePoolSize);
