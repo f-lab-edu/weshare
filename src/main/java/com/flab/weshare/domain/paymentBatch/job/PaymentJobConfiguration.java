44f29df836baa48cf9870b83232ca2217d30bc6c
goodchoi <jangu3384@gmail.com>
2024-04-19 15:53:47 +0900

    Feat: 결제 배치 job 파라미터 변수화로 동적 실행 기능 추가

--- src/main/java/com/flab/weshare/domain/paymentBatch/job/PaymentJobConfiguration.java
+++ src/main/java/com/flab/weshare/domain/paymentBatch/job/PaymentJobConfiguration.java
@@ -1,11 +1,18 @@
 package com.flab.weshare.domain.paymentBatch.job;
 
+import java.time.LocalDate;
+
 import org.springframework.batch.core.Job;
+import org.springframework.batch.core.configuration.annotation.JobScope;
 import org.springframework.batch.core.job.builder.JobBuilder;
 import org.springframework.batch.core.repository.JobRepository;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.transaction.PlatformTransactionManager;
+
+import com.flab.weshare.domain.party.entity.PartyCapsuleStatus;
+import com.flab.weshare.domain.paymentBatch.PayJobParameter;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -17,6 +24,14 @@ public class PaymentJobConfiguration {
 	private final PublishPaymentStepConfiguration publishPaymentStepConfiguration;
 	private final ExecutePaymentStepConfiguration executePaymentStepConfiguration;
 	private final UpdatePayResultStepConfiguration updatePayResultStepConfiguration;
+
+	@Bean
+	@JobScope
+	public PayJobParameter jobParameter(@Value("#{jobParameters[payDate]}") LocalDate payJobDate,
+		@Value("#{jobParameters[renewExpirationDate]}") LocalDate renewExpirationDate,
+		@Value("#{jobParameters[targetPartyCapsuleStatus]}") PartyCapsuleStatus status) {
+		return new PayJobParameter(payJobDate, renewExpirationDate, status);
+	}
 
 	@Bean
 	public Job initialPayJob(JobRepository jobRepository, PlatformTransactionManager platformTransactionManager) {
