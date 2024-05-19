606f6a7f5c4e3959808fdc3dd876b7bd20d7c496
goodchoi <jangu3384@gmail.com>
2024-05-20 01:13:39 +0900

    Test: 의도적 빌드 실패후 merge 테스트

--- src/main/java/com/flab/weshare/WeshareApplication.java
+++ src/main/java/com/flab/weshare/WeshareApplication.java
@@ -1,13 +1,12 @@
 package com.flab.weshare;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @EnableJpaAuditing
 @EnableScheduling
-@SpringBootApplication
+@SpringBootApplicatio
 public class WeshareApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(WeshareApplication.class, args);
