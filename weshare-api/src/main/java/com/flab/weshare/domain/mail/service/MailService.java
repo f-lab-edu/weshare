7077c165ae8ecc889bfd0ec66c5b0ff4466c436e
goodchoi <jangu3384@gmail.com>
2024-07-15 13:00:00 +0900

    Refactor: mail 관련 클래스 api모듈에서 분리

--- weshare-api/src/main/java/com/flab/weshare/domain/mail/service/MailService.java
+++
@@ -1,9 +0,0 @@
-package com.flab.weshare.domain.mail.service;
-
-import com.flab.weshare.domain.mail.dto.EmailDto;
-
-import jakarta.mail.MessagingException;
-
-public interface MailService {
-	void sendMail(EmailDto emailDto) throws MessagingException;
-}
