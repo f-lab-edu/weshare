d08bd3f7af579deb63b564e71235fe048024d368
goodchoi <jangu3384@gmail.com>
2024-07-26 13:00:00 +0900

    Chore: main 클래스 생성

---
+++ weshare-mail/src/main/java/com/flab/mail/WeshareMailApplication.java
@@ -0,0 +1,13 @@
+package com.flab.mail;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeshareMailApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WeshareMailApplication.class, args);
+	}
+
+}
