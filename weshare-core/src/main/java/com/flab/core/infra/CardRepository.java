ab11e70d436ef58197343cdd460208d693342857
goodchoi <jangu3384@gmail.com>
2024-08-06 13:00:00 +0900

    Feat: card id 구간조회 기능 추가

--- weshare-core/src/main/java/com/flab/core/infra/CardRepository.java
+++ weshare-core/src/main/java/com/flab/core/infra/CardRepository.java
@@ -1,8 +1,11 @@
 package com.flab.core.infra;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.flab.core.entity.Card;
 
 public interface CardRepository extends JpaRepository<Card, Long> {
+	List<Card> findByIdBetween(Long startId, Long endId);
 }
