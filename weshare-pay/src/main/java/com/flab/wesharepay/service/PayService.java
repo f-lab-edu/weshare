09a26ddf8cb59ab5df0f9369d9181d998737857e
goodchoi <jangu3384@gmail.com>
2024-09-11 00:00:00 +0900

    Feat: 기존 페이서비스 로직 모두 제거

--- weshare-pay/src/main/java/com/flab/wesharepay/service/PayService.java
+++ weshare-pay/src/main/java/com/flab/wesharepay/service/PayService.java
@@ -1,8 +1,6 @@
 package com.flab.wesharepay.service;
 
 public interface PayService {
-	String enrollCard(final CardInfo cardInfo, final Long userId);
-
 	Receipt payRequest(final String billingKey, final Integer amount, final Long orderId) throws
 		InterruptedException;
 }
--- weshare-pay/src/main/java/com/flab/wesharepay/service/PaymentProcessor.java
+++ weshare-pay/src/main/java/com/flab/wesharepay/service/PaymentProcessor.java
@@ -1,7 +1,5 @@
 package com.flab.wesharepay.service;
 
 public interface PaymentProcessor {
-	String requestBillingKey(CardInfo cardEnrollRequest, Long userId);
-
 	Receipt requestPayment(String billingKey, Integer amount, Long orderId);
 }
