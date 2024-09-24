d19c115f18ee27305abe4e72fd3407d3e756e151
goodchoi <jangu3384@gmail.com>
2024-09-24 13:00:00 +0900

    Feat : 닉네임 중복 조회 로직 및 이메일 중복 로직

--- weshare-api/src/main/java/com/flab/weshare/domain/user/service/UserService.java
+++ weshare-api/src/main/java/com/flab/weshare/domain/user/service/UserService.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.flab.core.infra.UserRepository;
+import com.flab.weshare.domain.user.dto.DuplicateCheckResponse;
 import com.flab.weshare.domain.user.dto.SignUpRequest;
 import com.flab.weshare.exception.ErrorCode;
 import com.flab.weshare.exception.exceptions.DuplicateException;
@@ -29,5 +30,17 @@ public class UserService {
 		}
 		String encodedPassword = passwordEncoder.encode(signUpRequest.password());
 		userRepository.save(signUpRequest.convert(encodedPassword));
+	}
+
+	@Transactional(readOnly = true)
+	public DuplicateCheckResponse emailDuplicateCheck(String email) {
+		boolean existsByEmail = userRepository.existsByEmail(email);
+		return new DuplicateCheckResponse(email, existsByEmail);
+	}
+
+	@Transactional(readOnly = true)
+	public DuplicateCheckResponse nickNameDuplicateCheck(String nickName) {
+		boolean existsByNickName = userRepository.existsByNickName(nickName);
+		return new DuplicateCheckResponse(nickName, existsByNickName);
 	}
 }
