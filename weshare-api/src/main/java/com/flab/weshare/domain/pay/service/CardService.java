da6e24fb7298c4be65bb125add61f8e532f6f22d
goodchoi <jangu3384@gmail.com>
2024-09-28 00:00:00 +0900

    Feat: 빌링키 발행 비즈니스 로직 구현

--- weshare-api/src/main/java/com/flab/weshare/domain/pay/service/CardService.java
+++ weshare-api/src/main/java/com/flab/weshare/domain/pay/service/CardService.java
@@ -1,14 +1,13 @@
 package com.flab.weshare.domain.pay.service;
 
+import org.json.simple.JSONObject;
 import org.springframework.stereotype.Service;
 
-import com.flab.core.entity.Card;
-import com.flab.core.infra.CardRepository;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.flab.core.entity.BillingKey;
+import com.flab.core.infra.BillingKeyRepository;
 import com.flab.core.infra.UserRepository;
-import com.flab.weshare.domain.pay.dto.CardEnrollRequest;
-import com.flab.weshare.utils.AesBytesEncryptUtil;
-import com.flab.wesharepay.service.CardInfo;
-import com.flab.wesharepay.service.PayService;
 
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
@@ -19,23 +18,24 @@ import lombok.extern.slf4j.Slf4j;
 @RequiredArgsConstructor
 public class CardService {
 	private final UserRepository userRepository;
-	private final PayService payService;
-	private final AesBytesEncryptUtil aesBytesEncryptUtil;
-	private final CardRepository cardRepository;
+	private final BillingKeyRepository billingKeyRepository;
+	private final ObjectMapper objectMapper;
 
 	@Transactional
-	public Long enrollCard(final CardEnrollRequest cardEnrollRequest, final Long userId) {
-		String billingKey = payService.enrollCard(generateCardInfo(cardEnrollRequest), userId);
-		String encryptCardNumber = aesBytesEncryptUtil.encrypt(cardEnrollRequest.cardNumber());
-		Card newCard = Card.buildNewCard(billingKey, encryptCardNumber);
-		return cardRepository.save(newCard).getId();
-	}
-
-	private CardInfo generateCardInfo(CardEnrollRequest cardEnrollRequest) {
-		return new CardInfo(cardEnrollRequest.cardNumber()
-			, cardEnrollRequest.cardPw()
-			, cardEnrollRequest.cardExpireYear()
-			, cardEnrollRequest.cardExpireMonth()
-			, cardEnrollRequest.birthDate());
+	public Long enrollBillingKey(final JSONObject billingKeyResponse) {
+		try {
+			BillingKey billingKey = objectMapper.readValue(billingKeyResponse.toJSONString(), BillingKey.class);
+			log.info("billingKey : {}", billingKey);
+			billingKey.setUser(
+				userRepository.findByClientId(billingKeyResponse.get("customerKey").toString())
+					.orElseThrow(() -> new RuntimeException("User not found")
+					));
+			billingKeyRepository.save(billingKey);
+			return billingKey.getId();
+		} catch (JsonProcessingException e) {
+			//Todo 로그 남기고 Exception handler에서 처리하도록 변경
+			log.error("billingKeyResponse json parsing error : {}", e.getMessage());
+			throw new RuntimeException(e);
+		}
 	}
 }
