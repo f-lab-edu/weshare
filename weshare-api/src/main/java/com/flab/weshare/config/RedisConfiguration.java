0b3ad9ee625975a00175448016aeff3ca1b77627
goodchoi <jangu3384@gmail.com>
2024-10-03 13:00:00 +0900

    Feat: api 모듈 Redis 설정 빈 등록

--- weshare-api/src/main/java/com/flab/weshare/config/RedisConfiguration.java
+++ weshare-api/src/main/java/com/flab/weshare/config/RedisConfiguration.java
@@ -6,15 +6,23 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import lombok.RequiredArgsConstructor;
+
 @Configuration
+@RequiredArgsConstructor
 public class RedisConfiguration {
 	@Value("${spring.data.redis.port}")
 	private int port;
 
 	@Value("${spring.data.redis.host}")
 	private String host;
+
+	private final ObjectMapper objectMapper;
 
 	@Bean
 	public RedisConnectionFactory redisConnectionFactory() {
@@ -27,6 +35,16 @@ public class RedisConfiguration {
 		redisTemplate.setKeySerializer(new StringRedisSerializer());
 		redisTemplate.setValueSerializer(new StringRedisSerializer());
 		redisTemplate.setConnectionFactory(redisConnectionFactory());
+		return redisTemplate;
+	}
+
+	@Bean
+	public RedisTemplate<String, Object> redisObjectTemplate() {
+		RedisTemplate<String, Object> redisTemplate = new RedisTemplate<>();
+		redisTemplate.setConnectionFactory(redisConnectionFactory());
+		redisTemplate.setKeySerializer(new StringRedisSerializer());
+		redisTemplate.setValueSerializer(
+			new Jackson2JsonRedisSerializer<>(objectMapper, String.class));
 		return redisTemplate;
 	}
 }
