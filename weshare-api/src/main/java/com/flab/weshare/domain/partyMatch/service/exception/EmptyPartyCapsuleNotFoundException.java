1ba21685989b14ff7e6007e550e96f85636adcc0
goodchoi <jangu3384@gmail.com>
2024-10-10 00:00:00 +0900

    Feat:: 커스텀 예외 처리

---
+++ weshare-api/src/main/java/com/flab/weshare/domain/partyMatch/service/exception/EmptyPartyCapsuleNotFoundException.java
@@ -0,0 +1,7 @@
+package com.flab.weshare.domain.partyMatch.service.exception;
+
+public class EmptyPartyCapsuleNotFoundException extends RuntimeException {
+	public EmptyPartyCapsuleNotFoundException(String message) {
+		super(message);
+	}
+}
