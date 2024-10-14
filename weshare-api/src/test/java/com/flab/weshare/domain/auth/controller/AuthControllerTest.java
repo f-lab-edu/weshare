0d5a6b0546e18d213c080b6810f3a4891cbaa336
goodchoi <jangu3384@gmail.com>
2024-10-14 13:00:00 +0900

    Test: AuthTest RestDocsToSwagger 구현

--- weshare-api/src/test/java/com/flab/weshare/domain/auth/controller/AuthControllerTest.java
+++ weshare-api/src/test/java/com/flab/weshare/domain/auth/controller/AuthControllerTest.java
@@ -1,13 +1,21 @@
 package com.flab.weshare.domain.auth.controller;
 
 import static com.flab.weshare.domain.utils.TestUtil.*;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.*;
+import static org.springframework.restdocs.payload.JsonFieldType.*;
+import static org.springframework.restdocs.payload.PayloadDocumentation.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.http.MediaType;
 
+import com.epages.restdocs.apispec.FieldDescriptors;
+import com.epages.restdocs.apispec.HeaderDescriptorWithType;
+import com.epages.restdocs.apispec.MockMvcRestDocumentationWrapper;
+import com.epages.restdocs.apispec.ResourceDocumentation;
+import com.epages.restdocs.apispec.ResourceSnippetParameters;
+import com.epages.restdocs.apispec.SimpleType;
 import com.flab.weshare.domain.auth.dto.LoginRequest;
 import com.flab.weshare.domain.base.BaseControllerTest;
 import com.flab.weshare.exception.ErrorCode;
@@ -17,6 +25,32 @@ public class AuthControllerTest extends BaseControllerTest {
 	LoginRequest rightloginRequest = new LoginRequest(EMAIL, PASSWORD);
 	LoginRequest wrongPasswordLoginRequest = new LoginRequest(EMAIL, PASSWORD + "1");
 
+	FieldDescriptors loginRequest = new FieldDescriptors()
+		.and(fieldWithPath("email").description("로그인 이메일"))
+		.and(fieldWithPath("password").description("비밀번호"));
+
+	FieldDescriptors basicResponseFields = new FieldDescriptors()
+		.and(fieldWithPath("success").type(SimpleType.BOOLEAN).description("반환 성공여부"))
+		.and(subsectionWithPath("data").type(OBJECT).description("반환 데이터").optional())
+		.and(fieldWithPath("errorMessage").type(OBJECT).description("에러 메시지").optional());
+
+	FieldDescriptors loginResponse = new FieldDescriptors()
+		.and(fieldWithPath("success").type(SimpleType.BOOLEAN).description("반환 성공여부"))
+		.and(subsectionWithPath("data").type(OBJECT).description("반환 데이터").optional())
+		.and(fieldWithPath("errorMessage").type(OBJECT).description("에러 메시지").optional())
+		.and(fieldWithPath("data.accessToken").description("jwt access token"))
+		.and(fieldWithPath("data.refreshToken").description("jwt refresh token"));
+
+	FieldDescriptors testResponse = new FieldDescriptors()
+		.and(fieldWithPath("success").type(SimpleType.BOOLEAN).description("반환 성공여부"))
+		.and(subsectionWithPath("data").type(SimpleType.STRING).description("access token").optional())
+		.and(fieldWithPath("errorMessage").type(OBJECT).description("에러 메시지").optional());
+
+	HeaderDescriptorWithType refreshTokenHeader =
+		new HeaderDescriptorWithType("Authorization")
+			.type(SimpleType.STRING)
+			.description("jwt Refresh token");
+
 	@DisplayName("로그인 성공시 액세스 토큰과 리프레시 토큰 반환")
 	@Test
 	void login_success() throws Exception {
@@ -25,7 +59,18 @@ public class AuthControllerTest extends BaseControllerTest {
 				.content(objectMapper.writeValueAsString(rightloginRequest)))
 			.andExpect(status().isOk())
 			.andExpect(jsonPath("$.data.accessToken").exists())
-			.andExpect(jsonPath("$.data.refreshToken").exists());
+			.andExpect(jsonPath("$.data.refreshToken").exists())
+			.andDo(MockMvcRestDocumentationWrapper.document("로그인 성공",
+				preRequestProcessorWithAuthorization,
+				preResponseProcessor
+				, ResourceDocumentation.resource(
+					ResourceSnippetParameters.builder()
+						.tag("인증 API")
+						.description("로그인")
+						.requestFields(loginRequest)
+						.responseFields(loginResponse)
+						.build()
+				)));
 	}
 
 	@DisplayName("로그인 실패 - 비밀번호 오류")
@@ -46,7 +91,18 @@ public class AuthControllerTest extends BaseControllerTest {
 	void logout_success() throws Exception {
 		mockMvc.perform(post("/api/logout")
 				.header(JwtProperties.HEADER, REFRESH_TOKEN))
-			.andExpect(status().isOk());
+			.andExpect(status().isOk())
+			.andDo(MockMvcRestDocumentationWrapper.document("로그아웃 성공",
+				preRequestProcessorWithAuthorization,
+				preResponseProcessor
+				, ResourceDocumentation.resource(
+					ResourceSnippetParameters.builder()
+						.tag("인증 API")
+						.description("로그아웃")
+						.requestHeaders(refreshTokenHeader)
+						.responseFields(basicResponseFields)
+						.build()
+				)));
 	}
 
 	@DisplayName("logout 요청시 Authorization 헤더가 존재하지 않으면 401 반환")
@@ -63,7 +119,18 @@ public class AuthControllerTest extends BaseControllerTest {
 				.header(JwtProperties.HEADER, REFRESH_TOKEN))
 			.andExpect(status().isOk())
 			.andExpect(jsonPath("$.data.accessToken").exists())
-			.andExpect(jsonPath("$.data.refreshToken").exists());
+			.andExpect(jsonPath("$.data.refreshToken").exists())
+			.andDo(MockMvcRestDocumentationWrapper.document("재발급 성공",
+				preRequestProcessorWithAuthorization,
+				preResponseProcessor
+				, ResourceDocumentation.resource(
+					ResourceSnippetParameters.builder()
+						.tag("인증 API")
+						.description("jwt토큰 재발급")
+						.requestHeaders(refreshTokenHeader)
+						.responseFields(loginResponse)
+						.build()
+				)));
 	}
 
 	@DisplayName("reissue 요청시 Authorization 헤더가 존재하지 않으면 401 반환")
