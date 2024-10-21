e1d6f10df277f34fdb87961ed2c7d4acbdf82bd3
goodchoi <jangu3384@gmail.com>
2024-10-21 13:00:00 +0900

    Refactor: 메일 폼 형성 기능 수정

--- weshare-mail/src/main/java/com/flab/mail/mail/view/MailConstructor.java
+++ weshare-mail/src/main/java/com/flab/mail/mail/view/MailConstructor.java
@@ -1,10 +1,11 @@
 package com.flab.mail.mail.view;
 
+import com.flab.mail.mail.dto.AccountInfoMailDto;
 import com.flab.mail.mail.dto.EmailDto;
 import com.flab.mail.mail.dto.SuccessPartyExtensionMailDto;
 
 public interface MailConstructor {
 	EmailDto constructRegularPaidMail(SuccessPartyExtensionMailDto successPartyExtensionMailDto);
 
-	//EmailDto constructJoinPaidMail();
+	EmailDto constructOttAccountInfoMail(AccountInfoMailDto accountInfoMailDto);
 }
--- weshare-mail/src/main/java/com/flab/mail/mail/view/MailConstructorImpl.java
+++ weshare-mail/src/main/java/com/flab/mail/mail/view/MailConstructorImpl.java
@@ -4,6 +4,7 @@ import org.springframework.stereotype.Component;
 import org.thymeleaf.context.Context;
 import org.thymeleaf.spring6.SpringTemplateEngine;
 
+import com.flab.mail.mail.dto.AccountInfoMailDto;
 import com.flab.mail.mail.dto.EmailDto;
 import com.flab.mail.mail.dto.SuccessPartyExtensionMailDto;
 
@@ -12,6 +13,8 @@ import lombok.RequiredArgsConstructor;
 @Component
 @RequiredArgsConstructor
 public class MailConstructorImpl implements MailConstructor {
+	private static final String DTO_NAME = "mailInfo";
+
 	private final SpringTemplateEngine templateEngine;
 
 	@Override
@@ -20,13 +23,23 @@ public class MailConstructorImpl implements MailConstructor {
 			.toAddress(successPartyExtensionMailDto.emailAddress())
 			.isHtml(true)
 			.subject("Weshare Ott 구독 결제 승인 안내")
-			.body(createRegularPaidMailBody(successPartyExtensionMailDto))
+			.body(createBodyByTemplateEngine(successPartyExtensionMailDto, "partyExtensionMail"))
 			.build();
 	}
 
-	private String createRegularPaidMailBody(SuccessPartyExtensionMailDto successPartyExtensionMailDto) {
+	@Override
+	public EmailDto constructOttAccountInfoMail(AccountInfoMailDto accountInfoMailDto) {
+		return EmailDto.builder()
+			.toAddress(accountInfoMailDto.email())
+			.isHtml(true)
+			.subject("Weshare Ott 계정 정보 발송")
+			.body(createBodyByTemplateEngine(accountInfoMailDto, "ottAccountInfoMail"))
+			.build();
+	}
+
+	private String createBodyByTemplateEngine(Object value, String templateName) {
 		Context context = new Context();
-		context.setVariable("mailInfo", successPartyExtensionMailDto);
-		return templateEngine.process("partyExtensionMail", context);
+		context.setVariable(DTO_NAME, value);
+		return templateEngine.process(templateName, context);
 	}
 }
