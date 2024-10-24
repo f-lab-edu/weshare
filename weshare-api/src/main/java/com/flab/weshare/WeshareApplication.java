39f7504c05ed66354106f74c365b5b1e30095a17
goodchoi <jangu3384@gmail.com>
2024-10-25 00:00:00 +0900

    Feat: api 모듈 스캔 범위 축소

--- weshare-api/src/main/java/com/flab/weshare/WeshareApplication.java
+++ weshare-api/src/main/java/com/flab/weshare/WeshareApplication.java
@@ -3,13 +3,23 @@ package com.flab.weshare;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.Import;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @EnableJpaAuditing
 @EntityScan("com.flab.core")
 @EnableJpaRepositories("com.flab.core")
-@SpringBootApplication(scanBasePackages = {"com.flab"})
+@EnableCaching
+@Import({
+	com.flab.mail.mail.service.MailPublisher.class
+})
+@SpringBootApplication(scanBasePackages = {
+	"com.flab.weshare",
+	"com.flab.core",
+	"com.flab.wesharepay"
+})
 public class WeshareApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(WeshareApplication.class, args);
